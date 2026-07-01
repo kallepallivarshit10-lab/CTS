43b17a2cf5aab1ccf87f04f25e0cd0e7c7604c15
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-01 19:38:07 +0530

    Add FactoryTest class for document creation

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_2_FactoryMethodPattern/FactoryTest.java
@@ -0,0 +1,22 @@
+public class FactoryTest {
+
+    public static void main(String[] args) {
+
+        DocumentFactory wordFactory =
+                new WordDocumentFactory();
+
+        DocumentFactory pdfFactory =
+                new PdfDocumentFactory();
+
+        DocumentFactory excelFactory =
+                new ExcelDocumentFactory();
+
+        Document doc1 = wordFactory.createDocument();
+        Document doc2 = pdfFactory.createDocument();
+        Document doc3 = excelFactory.createDocument();
+
+        doc1.open();
+        doc2.open();
+        doc3.open();
+    }
+}
