eeb9b368392569664aab565db9b93b066f985215
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 19:34:48 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_8_StrategyPattern/PayPalPayment.java
@@ -0,0 +1,11 @@
+public class PayPalPayment implements PaymentStrategy {
+
+    @Override
+    public void pay(double amount) {
+
+        System.out.println(
+                "Paid Rs." + amount +
+                " using PayPal");
+
+    }
+}
