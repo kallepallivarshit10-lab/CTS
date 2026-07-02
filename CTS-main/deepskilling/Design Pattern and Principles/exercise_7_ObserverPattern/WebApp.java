df402f587a9e8d7be3868827f889c8f8356d7bf7
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 19:30:20 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_7_ObserverPattern/WebApp.java
@@ -0,0 +1,12 @@
+public class WebApp implements Observer {
+
+    @Override
+    public void update(double price) {
+
+        System.out.println(
+                "Web App: Stock price updated to Rs."
+                        + price);
+
+    }
+
+}
