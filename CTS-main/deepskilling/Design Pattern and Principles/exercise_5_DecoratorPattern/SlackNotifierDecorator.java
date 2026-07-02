43772c0357f363e31da5802ad92e13b66a13de91
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 18:42:26 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_5_DecoratorPattern/SlackNotifierDecorator.java
@@ -0,0 +1,18 @@
+public class SlackNotifierDecorator extends NotifierDecorator {
+
+    public SlackNotifierDecorator(Notifier notifier) {
+
+        super(notifier);
+
+    }
+
+    @Override
+    public void send(String message) {
+
+        notifier.send(message);
+
+        System.out.println("Sending Slack Notification: " + message);
+
+    }
+
+}
