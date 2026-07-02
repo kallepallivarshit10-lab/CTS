7cacf383d4bd3b761675958a16dd7eaad6e74449
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 19:25:11 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_7_ObserverPattern/MobileApp.java
@@ -0,0 +1,12 @@
+public class MobileApp implements Observer {
+
+    @Override
+    public void update(double price) {
+
+        System.out.println(
+                "Mobile App: Stock price updated to Rs."
+                        + price);
+
+    }
+
+}
