817fde51241401974f991e224e9d505d7030a19b
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 17:57:33 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_4_AdapterPattern/StripeGateway.java
@@ -0,0 +1,10 @@
+public class StripeGateway {
+
+    public void makePayment(double amount) {
+
+        System.out.println("Payment of Rs." + amount +
+                           " processed through Stripe.");
+
+    }
+
+}
