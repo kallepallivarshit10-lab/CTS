4c0b5a2ade01b0af31e319618f403114fa392f40
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-02 19:53:57 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Design Pattern and Principles/exercise_11_DependencyInjection/CustomerRepositoryImpl.java
@@ -0,0 +1,12 @@
+public class CustomerRepositoryImpl
+        implements CustomerRepository {
+
+    @Override
+    public String findCustomerById(int id) {
+
+        return "Customer ID: " + id +
+               ", Name: Jyothirmai";
+
+    }
+
+}
