edcc4c71dfbeb0b17932dcadc4703bb63d351f5e
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-03 17:10:47 +0530

    Add files via upload
    Implement basic inventory management functionality including adding, updating, and deleting products.

---
+++ CTS-main/deepskilling/Data Structures and Algorithms/InventoryManagementSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Inventory inventory = new Inventory();
+
+        Product p1 = new Product(101, "Laptop", 20, 55000);
+
+        Product p2 = new Product(102, "Mouse", 100, 500);
+
+        inventory.AddProduct(p1);
+        inventory.AddProduct(p2);
+
+        Console.WriteLine();
+
+        inventory.DisplayProducts();
+
+        inventory.UpdateProduct(101, 30);
+
+        Console.WriteLine();
+
+        inventory.DisplayProducts();
+
+        inventory.DeleteProduct(102);
+
+        Console.WriteLine();
+
+        inventory.DisplayProducts();
+    }
+}
