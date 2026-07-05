a2eba3427c028d2b059a298174888527160a8d6c
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-05 13:24:35 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Data Structures and Algorithms/exercise_7_FinancialForecasting/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double currentValue = 1000;
+
+        double growthRate = 0.10;
+
+        int years = 3;
+
+        double futureValue =
+            Forecast.CalculateFutureValue(
+                currentValue,
+                growthRate,
+                years);
+
+        Console.WriteLine("Current Value : " + currentValue);
+
+        Console.WriteLine("Growth Rate : " + (growthRate * 100) + "%");
+
+        Console.WriteLine("Years : " + years);
+
+        Console.WriteLine("Future Value : " + futureValue);
+    }
+}
