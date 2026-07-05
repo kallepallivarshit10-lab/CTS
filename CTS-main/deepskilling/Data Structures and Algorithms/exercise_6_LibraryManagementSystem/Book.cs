f9b090545ec9f60c7f00942dfecd82b6e5796354
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-05 12:04:23 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Data Structures and Algorithms/exercise_6_LibraryManagementSystem/Book.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class Book
+{
+    public int BookId { get; set; }
+
+    public string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public Book(int bookId, string title, string author)
+    {
+        BookId = bookId;
+        Title = title;
+        Author = author;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(BookId + " " + Title + " " + Author);
+    }
+}
