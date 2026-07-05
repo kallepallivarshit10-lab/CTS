91a71c83f9e5fa6227c7d20125fec8d9b6ddf788
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-05 11:32:34 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Data Structures and Algorithms/exercise_5_TaskManagementSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        TaskLinkedList list = new TaskLinkedList();
+
+        list.AddTask(new Task(101, "Design UI", "Pending"));
+        list.AddTask(new Task(102, "Write Code", "In Progress"));
+        list.AddTask(new Task(103, "Testing", "Completed"));
+
+        list.TraverseTasks();
+
+        Console.WriteLine();
+
+        list.SearchTask(102);
+
+        Console.WriteLine();
+
+        list.DeleteTask(102);
+
+        list.TraverseTasks();
+    }
+}
