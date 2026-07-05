b3fceef6e6e43472615fd61b64767aa1fbfb5ccf
Varshit_Varma <kallepallivarshit10@gmail.com>
2026-07-05 10:53:09 +0530

    Add files via upload

---
+++ CTS-main/deepskilling/Data Structures and Algorithms/exercise_4_EmployeeManagementSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        EmployeeManagement management = new EmployeeManagement();
+
+        management.AddEmployee(new Employee(101, "Rahul", "Manager", 50000));
+        management.AddEmployee(new Employee(102, "Priya", "Developer", 40000));
+        management.AddEmployee(new Employee(103, "Arjun", "Tester", 35000));
+
+        management.TraverseEmployees();
+
+        Console.WriteLine();
+
+        management.SearchEmployee(102);
+
+        Console.WriteLine();
+
+        management.DeleteEmployee(102);
+
+        management.TraverseEmployees();
+    }
+}
